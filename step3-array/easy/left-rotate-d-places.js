be5be7a8cf63c7aa1b2772feb72f58a379be1925
Subham Rout <subhamroutnuagaon@gmail.com>
2023-04-02 10:33:46 +0530

    add optimal solution for left and right rotation d places of array

--- step3-array/easy/left-rotate-d-places.js
+++ step3-array/easy/left-rotate-d-places.js
@@ -7,7 +7,7 @@ for (let i = 0; i < length; i++) {
     arr.push(parseInt(prompt("Enter the number: ")));
 }
 
-//defining function(brute force) time complexity=O(d)+O(n-d)+O(d)=O(d+n-d+d)=O(d+n) and space complexity=O(d)
+//defining function(brute force)
 function leftRotateDPlaces(arr, length, D) {
     D = D % length;
     //push to temporary array
@@ -32,7 +32,25 @@ function leftRotateDPlaces(arr, length, D) {
     // }
 }
 
+//optimal solution
+function reverse(arr, start, end) {
+    while (start < end) {
+        let temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
+    }
+}
+function leftRotateDPlacesOptimal(arr, l, d) {
+    d = d % l;
+    reverse(arr, 0, d - 1);
+    reverse(arr, d, l - 1);
+    reverse(arr, 0, l - 1);
+}
+
 //output
 let D = parseInt(prompt("How many left places you want to rotate: "));
-leftRotateDPlaces(arr, length, D);
+// leftRotateDPlaces(arr, length, D);    //time complexity=O(d)+O(n-d)+O(d)=O(d+n-d+d)=O(d+n) and space complexity=O(d)
+leftRotateDPlacesOptimal(arr, length, D);  //time coplexity=O(d)+O(l-d)+O(l)=O(2l)=O(2n) and space complexity=O(1)
 console.log(`Array after rotating ${D} places : `, arr);
--- step3-array/easy/right-rotate-d-places.js
+++ step3-array/easy/right-rotate-d-places.js
@@ -7,8 +7,8 @@ for (let i = 0; i < length; i++) {
     arr.push(parseInt(prompt("Enter the number: ")));
 }
 
-//defining function(brute force) time complexity=O(d)+O(n-d)+O(d)=O(d+n-d+d)=O(d+n) and space complexity=O(d)
-function leftRotateDPlaces(arr, length, D) {
+//defining function(brute force)
+function rightRotateDPlaces(arr, length, D) {
     D = D % length;
     //push to temporary array
     let temp = [];
@@ -25,7 +25,24 @@ function leftRotateDPlaces(arr, length, D) {
     }
 }
 
+//optimal solution
+function reverse(arr, start, end) {
+    while (start < end) {
+        let temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
+    }
+}
+function rightRotateDPlacesOptimal(arr, n, d) {
+    d = d % n;
+    reverse(arr, 0, n - 1 - d);
+    reverse(arr, n - d, n - 1);
+    reverse(arr, 0, n - 1);
+}
 //output
 let D = parseInt(prompt("How many right places you want to rotate: "));
-leftRotateDPlaces(arr, length, D);
-console.log(`Array after rotating ${D} places : `, arr);
+// rightRotateDPlaces(arr, length, D);   //time complexity=O(d)+O(n-d)+O(d)=O(d+n-d+d)=O(d+n) and space complexity=O(d)
+rightRotateDPlacesOptimal(arr, length, D);  //time complexity=O(n-d)+O(d)+O(n)=O(2n) and space complexity=O(1)
+console.log(`Array after rotating ${D} places from right : `, arr);
