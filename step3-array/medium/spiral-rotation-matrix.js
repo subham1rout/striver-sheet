8639ab554cb5290d78a1b233823a8a994bd172a8
Subham Rout <subhamroutnuagaon@gmail.com>
2023-05-21 00:14:02 +0530

    add spiral rotation of matrix problem

---
+++ step3-array/medium/spiral-rotation-matrix.js
@@ -0,0 +1,53 @@
+const prompt = require("prompt-sync")({ sigint: true });
+
+//matrix input
+let m = parseInt(prompt("enter row length: "));
+let n = parseInt(prompt("enter column length: "));
+let matrix = [];
+for (let i = 0; i < m; i++) {
+    console.log(`enter ${i + 1} rows element:`);
+    let temp = [];
+    for (let j = 0; j < n; j++) {
+        temp.push(parseInt(prompt()));
+    }
+    matrix.push(temp);
+}
+
+console.log("matrix is ", matrix);
+
+//optimal solution -> time=O(m*n) and space=O(m*n)
+function spiralRotationMatrix(matrix) {
+    let m = matrix.length;
+    let n = matrix[0].length;
+    let top = 0;
+    let bottom = m - 1;
+    let left = 0;
+    let right = n - 1;
+    let ans = [];
+    while (top <= bottom && left <= right) {
+        for (let i = left; i <= right; i++) {
+            ans.push(matrix[top][i]);
+        }
+        top++;
+        for (let i = top; i <= bottom; i++) {
+            ans.push(matrix[i][right]);
+        }
+        right--;
+        if (top <= bottom) {
+            for (let i = right; i >= left; i--) {
+                ans.push(matrix[bottom][i]);
+            }
+            bottom--;
+        }
+        if (left <= right) {
+            for (let i = bottom; i >= top; i--) {
+                ans.push(matrix[i][left]);
+            }
+            left++;
+        }
+    }
+    return ans;
+}
+
+let ans = spiralRotationMatrix(matrix);
+console.log("Spiral print of the matrix is ", ans);
