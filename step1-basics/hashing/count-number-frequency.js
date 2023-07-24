aead2a5c8ee68cab6a14fc6bd7c85486f119d491
Subham Rout <subhamroutnuagaon@gmail.com>
2023-07-24 11:45:24 +0530

    add revision for hashing

--- step1-basics/hashing/count-number-frequency.js
+++ step1-basics/hashing/count-number-frequency.js
@@ -24,3 +24,24 @@ function countFrequency(inputarr, number) {
     console.log(JSON.stringify(hasharray));
     return hasharray[number];
 }
+
+//revision-1
+const length = parseInt(prompt("enter the length of the array:"));
+const arr = [];
+for (let i = 0; i < length; i++) {
+    arr.push(parseInt(prompt("enter the number:")));
+}
+let max = arr[0];
+for (let i = 1; i < length; i++) {
+    if (max < arr[i]) max = arr[i];
+}
+const newarr = new Array(max + 1).fill(0);
+for (let i = 0; i < length; i++) {
+    newarr[arr[i]]++;
+}
+
+const query1 = parseInt(prompt('enter no of queries:'));
+for (let i = 0; i < query1; i++) {
+    let no = parseInt(prompt('enter the no which frequency you want:'));
+    console.log('frequency of ', no, ' is ', newarr[no]);
+}
--- step1-basics/hashing/highest-lowest-frequency.js
+++ step1-basics/hashing/highest-lowest-frequency.js
@@ -1,19 +1,19 @@
 const prompt = require("prompt-sync")({ sigint: true });
 
 //input array
-const arrlength = parseInt(prompt("Enter the array size:"));
-const arr = [];
-for (let i = 0; i < arrlength; i++) {
-    arr.push(parseInt(prompt("Enter the number:")));
-}
+// const arrlength = parseInt(prompt("Enter the array size:"));
+// const arr = [];
+// for (let i = 0; i < arrlength; i++) {
+//     arr.push(parseInt(prompt("Enter the number:")));
+// }
 
 //pre compute
-let max = Math.max(...arr);
-let hasharray = new Array(max + 1).fill(0);
-for (let i = 0; i < arrlength; i++) {
-    hasharray[arr[i]] += 1;
-}
-console.log("hash array:", hasharray);
+// let max = Math.max(...arr);
+// let hasharray = new Array(max + 1).fill(0);
+// for (let i = 0; i < arrlength; i++) {
+//     hasharray[arr[i]] += 1;
+// }
+// console.log("hash array:", hasharray);
 
 
 //get highest
@@ -26,7 +26,7 @@ function getHighestFrequency() {
     }
     return maxno;
 }
-console.log("Maximum Frequency in the array:", getHighestFrequency());
+// console.log("Maximum Frequency in the array:", getHighestFrequency());
 
 //get lowest
 function getLowestFrequency() {
@@ -38,4 +38,24 @@ function getLowestFrequency() {
     }
     return minno;
 }
-console.log("Maximum Frequency in the array:", getLowestFrequency());
+// console.log("Maximum Frequency in the array:", getLowestFrequency());
+
+
+//revision-1
+const length1 = parseInt(prompt('enter length of the array:'));
+const arr1 = [];
+for (let i = 0; i < length1; i++) {
+    arr1.push(parseInt(prompt('enter the number:')));
+}
+let max1 = Math.max(...arr1);
+let newarr1 = new Array(max1 + 1).fill(0);
+for (let i = 0; i < length1; i++) {
+    newarr1[arr1[i]]++;
+}
+let maxFreq = newarr1[1];
+let minFreq = newarr1[1];
+for (let i = 1; i < newarr1.length; i++) {
+    if (maxFreq < newarr1[i]) maxFreq = newarr1[i];
+    if (minFreq > newarr1[i]) minFreq = newarr1[i];
+}
+console.log("maximum frequncy=", newarr1.indexOf(maxFreq), "and minimum frequncy =", newarr1.indexOf(minFreq));
