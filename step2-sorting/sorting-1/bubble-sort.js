1acdd9932eef06714da13bbe3e05508c834cb154
Subham Rout <subhamroutnuagaon@gmail.com>
2023-07-26 00:01:40 +0530

    add bubble sort

--- step2-sorting/sorting-1/bubble-sort.js
+++ step2-sorting/sorting-1/bubble-sort.js
@@ -35,3 +35,28 @@ function bubbleSort(arr, n) {
 
 //time complexity= O(n^2) i.e n+ n-1 + n-2 + n-3 + n-4 +......+1=n(n+1)/2 =n^2  (average case, worst case)
 //time complexity= O(n) i.e if in first try swaping not happening just get out of loop (best case)
+
+//revision-1
+const length1 = parseInt(prompt('enter the length of the array:'));
+const arr1 = [];
+for (let i = 0; i < length1; i++) {
+    arr1.push(parseInt(prompt('enter the number:')));
+}
+
+function bubbleSort1(arr, n) {
+    let swap = 0;
+    for (let i = n - 2; i >= 0; i--) {
+        for (let j = 0; j <= i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                swap = 1;
+            }
+        }
+        if (swap == 0) break;
+    }
+    console.log("Bubble sort-> ", arr);
+}
+
+bubbleSort1(arr1, length1);
--- step2-sorting/sorting-1/selection-sort.js
+++ step2-sorting/sorting-1/selection-sort.js
@@ -1,19 +1,19 @@
 const prompt = require("prompt-sync")({ sigint: true });
 
 //problem input
-// const length = parseInt(prompt("Enter length of array you sort:"));
-// let arr = [];
-// for (let i = 0; i < length; i++) {
-//     arr.push(parseInt(prompt("Enter number:")));
-// }
+const length = parseInt(prompt("Enter length of array you sort:"));
+let arr = [];
+for (let i = 0; i < length; i++) {
+    arr.push(parseInt(prompt("Enter number:")));
+}
 //calling func
-// selectionSort(arr, length);
+selectionSort(arr, length);
 
 //printing output
-// console.log("Sorting of arr:");
-// for (let i = 0; i < length; i++) {
-//     console.log(arr[i]);
-// }
+console.log("Sorting of arr:");
+for (let i = 0; i < length; i++) {
+    console.log(arr[i]);
+}
 
 //defining function
 function selectionSort(arr, n) {
