a99a95f919342ec7193efa5818219d39e8e57fef
Subham Rout <subhamroutnuagaon@gmail.com>
2023-08-03 18:06:02 +0530

    add quick sort revision

--- step2-sorting/sorting-2/quick-sort.js
+++ step2-sorting/sorting-2/quick-sort.js
@@ -56,3 +56,48 @@ for (let i = 0; i < arrlength; i++) {
 
 //time complexity=O(nlogn)
 //space complexity=O(1)
+
+
+//revision-1
+
+const arrlength1 = parseInt(prompt("enter the length of the array:"));
+const arr1 = [];
+for (let i = 0; i < arrlength1; i++) {
+    arr1.push(parseInt(prompt("enter the number:")));
+}
+
+function partition1(arr, low, high) {
+    let pivot = arr[low];
+    let i = low;
+    let j = high;
+    while (i < j) {
+        while (arr[i] <= pivot && i <= high - 1) i++;
+        while (arr[j] > pivot && j >= low - 1) j--;
+        if (i < j) {
+            let temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+    let temp = arr[low];
+    arr[low] = arr[j];
+    arr[j] = temp;
+
+    return j;
+}
+
+function qS(arr, low, high) {
+    if (low < high) {
+        let partitionIndex1 = partition1(arr, low, high);
+        qS(arr, low, partitionIndex1 - 1);
+        qS(arr, partitionIndex1 + 1, high);
+    }
+}
+
+function quickSort1(arr, n) {
+    return qS(arr, 0, n - 1);
+}
+
+
+quickSort1(arr1, arrlength1);
+console.log(arr1);
