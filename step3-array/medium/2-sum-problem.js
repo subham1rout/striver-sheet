555ee285e1ddcc692775e1d6f58bcf684393c2ca
Subham Rout <subhamroutnuagaon@gmail.com>
2023-09-14 16:27:59 +0530

    add 2-sum problem

--- step3-array/medium/2-sum-problem.js
+++ step3-array/medium/2-sum-problem.js
@@ -21,9 +21,7 @@ function twoSumProblem(arr, target) {
     }
     return 'no';
 }
-
-let ans = twoSumProblem(arr, target);
-console.log(`In arr [ ${arr} ], 2 sum result: ${ans}`);
+// console.log(`In arr [ ${arr} ], 2 sum result: ${twoSumProblem(arr, target)}`);
 
 //optimize brute force -> time=o(n^2) and space=O(1)
 function twoSumProblem1(arr, target) {
@@ -37,9 +35,7 @@ function twoSumProblem1(arr, target) {
     }
     return 'not exist';
 }
-
-let ans1 = twoSumProblem1(arr, target);
-console.log(`In arr [ ${arr} ], 2 sum result: ${ans1}`);
+// console.log(`In arr [ ${arr} ], 2 sum result: ${twoSumProblem1(arr, target)}`);
 
 //better solution -> using hash map -> time=O(nlogn) and space=O(n)
 function twoSumProblem2(arr, target) {
@@ -55,9 +51,7 @@ function twoSumProblem2(arr, target) {
     }
     return 'not exist';
 }
-
-let ans2 = twoSumProblem2(arr, target);
-console.log(`In arr [ ${arr} ], 2 sum result: ${ans2}`);
+// console.log(`In arr [ ${arr} ], 2 sum result: ${twoSumProblem2(arr, target)}`);
 
 //optimal solution -> 2 pointer approach -> time=O(n) and space =O(1)
 function twoSumProblem3(arr, target) {
@@ -76,6 +70,49 @@ function twoSumProblem3(arr, target) {
     }
     return 'no';
 }
+// console.log(`In arr [ ${arr} ], 2 sum result optimal: ${twoSumProblem3(arr, target)}`);
 
-let ans3 = twoSumProblem3(arr, target);
-console.log(`In arr [ ${arr} ], 2 sum result optimal: ${ans3}`);    
+
+//revision-1
+//brute -> time=O(n^2) and space=O(1)
+function twoSum(arr, N, target) {
+    for (let i = 0; i < N; i++) {
+        for (let j = i + 1; j < N; j++) {
+            if (arr[i] + arr[j] == target) {
+                return [i, j];
+            }
+        }
+    }
+    return [-1, -1];
+}
+console.log("Result -> ", twoSum(arr, length, target));
+
+//better -> hashing -> time=O(nlogn) and space=O(n)
+function twoSum1(arr, N, target) {
+    let map = new Map();
+    for (let i = 0; i < N; i++) {
+        let value = target - arr[i];
+        if (map.has(value)) {
+            return [map.get(value), i];
+        } else {
+            map.set(arr[i], i);
+        }
+    }
+    return [-1, -1];
+}
+console.log("Result -> ", twoSum1(arr, length, target));
+
+//optimal -> used only for yes and no case -> 2 pointer -> greedy -> time=O(n) and space=O(1)
+function twoSum2(arr, N, target) {
+    arr = arr.sort((a, b) => a - b);
+    let left = 0;
+    let right = N - 1;
+    while (left < right) {
+        let sum = arr[left] + arr[right];
+        if (sum == target) return "yes";
+        else if (sum < target) left++;
+        else right--;
+    }
+    return "no";
+}
+console.log("Result -> ", twoSum2(arr, length, target));
