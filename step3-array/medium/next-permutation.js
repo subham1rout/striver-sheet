ee01f5db08d27a3d7d20d9e0793ed049b59931d3
Subham Rout <subhamroutnuagaon@gmail.com>
2023-10-06 00:29:21 +0530

    add next permutation problem

--- step3-array/medium/next-permutation.js
+++ step3-array/medium/next-permutation.js
@@ -54,4 +54,42 @@ function nextPermutation(arr) {
     return reverse(arr, index + 1, n - 1);
 }
 
-console.log(`Next pemutation for the array [ ${arr} ] is [ ${nextPermutation(arr)} ]`);
+// console.log(`Next pemutation for the array [ ${arr} ] is [ ${nextPermutation(arr)} ]`);
+
+
+//revision-1
+//optimal -> time=O(3n) and space=O(1)
+function reverse1(arr, start, end) {
+    while (start < end) {
+        let temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
+    }
+    console.log(arr);
+    return arr;
+}
+function nextPermutation1(arr, n) {
+    let index = -1;
+    for (let i = n - 2; i >= 0; i--) {
+        if (arr[i] < arr[i + 1]) {
+            index = i;
+            break;
+        }
+    }
+    if (index == -1) {
+        return reverse1(arr, 0, n - 1);
+    }
+    console.log(arr);
+    for (let i = n - 1; i >= index; i--) {
+        if (arr[i] > arr[index]) {
+            let temp = arr[i];
+            arr[i] = arr[index];
+            arr[index] = temp;
+            break;
+        }
+    }
+    return reverse1(arr, index + 1, n - 1);
+}
+console.log(`Next pemutation1 for the array [ ${arr} ] is [ ${nextPermutation1(arr, length)} ]`);
