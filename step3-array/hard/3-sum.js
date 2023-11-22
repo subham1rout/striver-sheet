2135eb3734fc371c9eb9c3a3aed6828a7ebfd8d4
Subham Rout <subhamroutnuagaon@gmail.com>
2023-11-22 22:33:26 +0530

    add better for 3-sum

--- step3-array/hard/3-sum.js
+++ step3-array/hard/3-sum.js
@@ -19,7 +19,6 @@ function find3Sum(arr, n) {
             }
         }
     }
-    console.log(set);
     let ans = [];
     for (let value of set) {
         ans.push(JSON.parse(value));
@@ -27,3 +26,25 @@ function find3Sum(arr, n) {
     return ans;
 }
 console.log("Unique 3 SUMs are ", find3Sum(arr, length));
+
+//better -> time=O(n^2*logn) and space=O(n)+O(2*n)
+function find3Sum1(arr, n) {
+    let set = new Set();
+    for (let i = 0; i < n; i++) {
+        let tempSet = new Set();
+        for (let j = i + 1; j < n; j++) {
+            let k = -(arr[i] + arr[j]);
+            if (tempSet.has(k)) {
+                let x = [arr[i], arr[j], k].sort((a, b) => a - b);
+                set.add(JSON.stringify(x));
+            }
+            tempSet.add(arr[j]);
+        }
+    }
+    let ans = [];
+    for (const value of set) {
+        ans.push(value);
+    }
+    return ans;
+}
+console.log("Unique 3 SUM1s are ", find3Sum1(arr, length));
