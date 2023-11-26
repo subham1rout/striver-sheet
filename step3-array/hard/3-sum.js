c218140bdeb01fb80058f493110e82d6f7410af9
Subham Rout <subhamroutnuagaon@gmail.com>
2023-11-26 23:00:31 +0530

    add 3-sum problem

--- step3-array/hard/3-sum.js
+++ step3-array/hard/3-sum.js
@@ -48,3 +48,30 @@ function find3Sum1(arr, n) {
     return ans;
 }
 console.log("Unique 3 SUM1s are ", find3Sum1(arr, length));
+
+//optimal -> 2 pointer approach -> time=O(nlogn)*O(n*n) and space=O(no of unique triplets)
+function find3Sum2(arr, n) {
+    let ans = [];
+    arr.sort((a, b) => a - b);
+    for (let i = 0; i < n; i++) {
+        if (i > 0 && arr[i - 1] == arr[i]) continue;
+        let j = i + 1;
+        let k = n - 1;
+        while (j < k) {
+            let sum = arr[i] + arr[j] + arr[k];
+            if (sum > 0) {
+                k--;
+            } else if (sum < 0) {
+                j++;
+            } else {
+                ans.push([arr[i], arr[j], arr[k]]);
+                j++;
+                k--;
+                while (j < k && arr[j] == arr[j - 1]) j++;
+                while (j < k && arr[k] == arr[k + 1]) k--;
+            }
+        }
+    }
+    return ans;
+}
+console.log("Unique 3 SUM2s are ", find3Sum2(arr, length));
