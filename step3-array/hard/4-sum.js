5143406f15d4b9b01d98372ba93b3d03eeb73dc8
Subham Rout <subhamroutnuagaon@gmail.com>
2023-12-20 12:39:23 +0530

    add 4sum optimal approach

--- step3-array/hard/4-sum.js
+++ step3-array/hard/4-sum.js
@@ -44,3 +44,35 @@ function find4Sum1(arr, n, target) {
     return ans;
 }
 console.log("4 Sum1 of the arr:", find4Sum1(arr, length, target));
+
+//best -> time=O(n^3) and space=O(no of unique quads)
+function find4Sum2(arr, n, target) {
+    arr = arr.sort((a, b) => a - b);
+    let ans = new Set();
+    for (let i = 0; i < n; i++) {
+        if (i > 0 && arr[i] == arr[i - 1]) continue;
+        for (let j = i + 1; j < n; j++) {
+            if (j > 1 && arr[j] == arr[j - 1]) continue;
+            let k = j + 1;
+            let l = n - 1;
+            while (k < l) {
+                let sum = arr[i] + arr[j];
+                sum = sum + arr[k];
+                sum = sum + arr[l];
+                if (sum == target) {
+                    ans.add(JSON.stringify([arr[i], arr[j], arr[k], arr[l]]));
+                    k++;
+                    l--;
+                } else if (sum > target) {
+                    l--;
+                } else k++;
+            }
+        }
+    }
+    let ansarr = [];
+    for (const el of ans) {
+        ansarr.push(JSON.parse(el));
+    }
+    return ansarr;
+}
+console.log("4 Sum2 of the arr:", find4Sum2(arr, length, target));
