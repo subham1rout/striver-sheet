92460997bf14704cd3a89a3cc63976f294a3bbbd
Subham Rout <subhamroutnuagaon@gmail.com>
2024-01-01 00:49:29 +0530

    add count reverse pairs

--- step3-array/hard/count-reverse-pairs.js
+++ step3-array/hard/count-reverse-pairs.js
@@ -7,31 +7,40 @@ for (let i = 0; i < length; i++) {
 }
 
 //brute -> time=O(n^2) and space=O(1)
-function countInversions(arr, length) {
+function countReversePairs(arr, length) {
     let count = 0;
     for (let i = 0; i < length; i++) {
         for (let j = i + 1; j < length; j++) {
-            if (arr[i] > arr[j]) {
+            if (arr[i] > 2 * arr[j]) {
                 count++;
             }
         }
     }
     return count;
 }
-console.log("Count of the inversions of the array:", countInversions(arr, length));
+console.log("Total Reverse pairs=", countReversePairs(arr, length));
 
-//optimal -> time=O(nlogn) and space=O(n)
+//optimal -> time=O(2nlogn) and space=O(n)
+function countPairs(arr, low, mid, high) {
+    let right = mid + 1;
+    let count = 0;
+    for (let i = low; i <= mid; i++) {
+        while (right <= high && arr[i] > 2 * arr[right]) {
+            right++;
+        }
+        count += right - mid - 1;
+    }
+    return count;
+}
 function merge(arr, low, mid, high) {
     let left = low;
     let right = mid + 1;
     let temp = [];
-    let count = 0;
     while (left <= mid && right <= high) {
         if (arr[left] <= arr[right]) {
             temp.push(arr[left]);
             left++;
         } else {
-            count += mid - left + 1;
             temp.push(arr[right]);
             right++;
         }
@@ -47,20 +56,18 @@ function merge(arr, low, mid, high) {
     for (let i = low; i <= high; i++) {
         arr[i] = temp[i - low];
     }
-    // console.log("low", low, "high", high, "count", count);
-    return count;
 }
 function mergeSort(arr, low, high) {
-    let count = 0;
     if (low >= high) return 0;
+    let count = 0;
     let mid = Math.floor((low + high) / 2);
     count += mergeSort(arr, low, mid);
     count += mergeSort(arr, mid + 1, high);
-    count += merge(arr, low, mid, high);
+    count += countPairs(arr, low, mid, high);
+    merge(arr, low, mid, high);
     return count;
 }
-function countInversions2(arr, length) {
-    let count = mergeSort(arr, 0, length - 1);
-    return count;
+function countReversePairs1(arr, length) {
+    return mergeSort(arr, 0, length - 1);
 }
-console.log("Count Inversion1 =", countInversions2(arr, length));
+console.log("Total Reverse pairs1=", countReversePairs1(arr, length));
