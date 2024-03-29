f153b749ec6a8b804bf190b8f9f5424b54cfb70c
Subham Rout <subhamroutnuagaon@gmail.com>
2024-03-29 22:10:48 +0530

    add 2nd brute of missing positive numbers

--- step4-bs/bs-answers/missing-positive-number.js
+++ step4-bs/bs-answers/missing-positive-number.js
@@ -7,6 +7,7 @@ for (let i = 0; i < length; i++) {
 }
 const k = parseInt(prompt("enter the kth position:"));
 
+//brute -> time=O(n) and space=o(n)
 function getMissingPositiveNumber(arr, n, k) {
     let arr2 = [];
     for (let i = 0; i < 100; i++) {
@@ -17,3 +18,21 @@ function getMissingPositiveNumber(arr, n, k) {
     return arr2[k];
 }
 console.log("Missing Number is ", getMissingPositiveNumber(arr, length, k));
+
+//2nd brute -> time=O(n) and space=O(1)
+function getMissingPositiveNumber1(arr, n, k) {
+    for (let i = 0; i < n; i++) {
+        if (arr[i] <= k) {
+            k++;
+        } else {
+            break;
+        }
+    }
+    return k;
+}
+console.log("Missing Number1 is ", getMissingPositiveNumber1(arr, length, k));
+
+//better
+function getMissingPositiveNumber2(arr, n, k) {
+    
+}
