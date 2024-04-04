7e7cdc15f5c9081144edaf0e01f91ec13549ac4a
Subham Rout <subhamroutnuagaon@gmail.com>
2024-04-04 23:40:10 +0530

    add brute for capcity to ship packages within D days

---
+++ step4-bs/bs-answers/capacity-ship-packages.js
@@ -0,0 +1,56 @@
+const prompt = require("prompt-sync")({ sigint: true });
+
+const length = parseInt(prompt("enter the length:"));
+const weights = [];
+for (let i = 0; i < length; i++) {
+    weights.push(parseInt(prompt("enter the weights:")));
+}
+const days = parseInt(prompt("enter the no of days assigned for shipping:"));
+
+//brute ->time=O(sum-max+1)*O(n) and space=O(1) 
+function getMax(weights) {
+    let max = weights[0];
+    for (let i = 0; i < weights.length; i++) {
+        if (weights[i] > max) {
+            max = weights[i];
+        }
+    }
+    return max;
+}
+
+function getSum(weights) {
+    // let last = weights[weights.length - 1];
+    // return Math.floor(last * (last + 1) / 2);
+    let sum=0;
+    for(let i=0;i<weights.length;i++){
+        sum+=weights[i];
+    }
+    return sum;
+}
+
+function getDays(weights, capacity) {
+    let load = 0;
+    let days = 1;
+    for (let i = 0; i < weights.length; i++) {
+        if (load + weights[i] <= capacity) {
+            load += weights[i];
+        } else {
+            days++;
+            load = weights[i];
+        }
+    }
+    return days;
+}
+
+function getLeastCapacity(weights, days) {
+    let max = getMax(weights);
+    let sum = getSum(weights);
+    for (let i = max; i <= sum; i++) {
+        let daysTaken = getDays(weights, i);
+        if (daysTaken <= days) {
+            return i;
+        }
+    }
+}
+
+console.log("Least capacity to ship packages = ",getLeastCapacity(weights,days));
