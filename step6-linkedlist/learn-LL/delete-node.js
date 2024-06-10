d4307989dfe732adc903790b07dfa96df9577234
Subham Rout <subhamroutnuagaon@gmail.com>
2024-06-10 22:09:09 +0530

    add delete node from front and back

--- step6-linkedlist/learn-LL/delete-node.js
+++ step6-linkedlist/learn-LL/delete-node.js
@@ -1,3 +1,4 @@
+//define node
 class Node {
     constructor(data1, next1) {
         this.data = data1;
@@ -5,6 +6,7 @@ class Node {
     }
 }
 
+//create the LL
 function insertNode(head, data) {
     let newnode = new Node(data);
     newnode.next = head;
@@ -12,16 +14,30 @@ function insertNode(head, data) {
     return head;
 }
 
-function deleteLastNode(head) {
-    let temp = head;
-    while (temp.next) {
-        console.log(temp.data);
-        temp = temp.next;
+//delete from first of LL
+function deleteFirstNode(head) {
+    if (head == undefined || head.next == undefined) {
+        return undefined;
     }
-    temp = undefined;
+    head = head.next;
     return head;
 }
 
+//delete from last in LL
+function deleteLastNode(head) {
+    if (head == undefined || head.next == undefined) {
+        return undefined;
+    }
+    let temp = head;
+    while (temp.next.next) {
+        temp = temp.next;
+    }
+    temp.next = undefined;
+    return head;
+}
+
+
+//execute func
 let head = undefined;
 head = insertNode(head, 1);
 head = insertNode(head, 2);
@@ -29,4 +45,5 @@ head = insertNode(head, 3);
 head = insertNode(head, 4);
 
 console.log("Before deleting last node from LL ->", JSON.stringify(head));
+console.log("After deleting first node from LL ->", JSON.stringify(deleteFirstNode(head)));
 console.log("After deleting last node from LL ->", JSON.stringify(deleteLastNode(head)));
