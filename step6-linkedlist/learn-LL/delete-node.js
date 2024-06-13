a2159d129ae7bf67c98902ac46dd4da5d654b161
Subham Rout <subhamroutnuagaon@gmail.com>
2024-06-13 23:01:14 +0530

    add insert and delete node

--- step6-linkedlist/learn-LL/delete-node.js
+++ step6-linkedlist/learn-LL/delete-node.js
@@ -1,3 +1,5 @@
+const prompt = require("prompt-sync")({ sigint: true });
+
 //define node
 class Node {
     constructor(data1, next1) {
@@ -6,7 +8,7 @@ class Node {
     }
 }
 
-//create the LL
+//create the LL 
 function insertNode(head, data) {
     let newnode = new Node(data);
     newnode.next = head;
@@ -14,7 +16,7 @@ function insertNode(head, data) {
     return head;
 }
 
-//delete from first of LL
+//delete from first of LL -> time=O(n) and space=O(1)
 function deleteFirstNode(head) {
     if (head == undefined || head.next == undefined) {
         return undefined;
@@ -23,7 +25,7 @@ function deleteFirstNode(head) {
     return head;
 }
 
-//delete from last in LL
+//delete from last in LL -> time=O(n) and space=O(1)
 function deleteLastNode(head) {
     if (head == undefined || head.next == undefined) {
         return undefined;
@@ -36,14 +38,60 @@ function deleteLastNode(head) {
     return head;
 }
 
+//delete from a kth position -> time=O(n) and space=O(1)
+function deleteFromKthPosition(head, k) {
+    if (head == undefined) {
+        return undefined;
+    }
+    if (k == 1) {
+        head = head.next;
+        return head;
+    }
+    let temp = head;
+    let count = 0;
+    let prev = undefined;
+    while (temp) {
+        count++;
+        if (count == k) {
+            prev.next = prev.next.next;
+            break;
+        }
+        prev = temp;
+        temp = temp.next;
+    }
+    return head;
+}
+
+//delete a value of LL -> time=O(n) and space=O(1)
+function deleteElement(head, value) {
+    if (head == undefined) return undefined;
+    if (head.data == value) {
+        head = head.next;
+        return head;
+    }
+    let temp = head;
+    let prev = undefined;
+    while (temp) {
+        if (temp.data == value) {
+            prev.next = prev.next.next;
+            break;
+        }
+        prev = temp;
+        temp = temp.next;
+    }
+    return head;
+}
 
 //execute func
 let head = undefined;
-head = insertNode(head, 1);
-head = insertNode(head, 2);
-head = insertNode(head, 3);
-head = insertNode(head, 4);
+let length = parseInt(prompt("enter the length of which you want LL:"));
+for (let i = 0; i < length; i++) {
+    head = insertNode(head, parseInt(prompt("enter value:")));
+}
+
 
 console.log("Before deleting last node from LL ->", JSON.stringify(head));
 console.log("After deleting first node from LL ->", JSON.stringify(deleteFirstNode(head)));
 console.log("After deleting last node from LL ->", JSON.stringify(deleteLastNode(head)));
+console.log("After deleting kth node from LL ->", JSON.stringify(deleteFromKthPosition(head, 2)));
+console.log("After deleting node having value from LL ->", JSON.stringify(deleteElement(head, 5)));
--- step6-linkedlist/learn-LL/insert-node.js
+++ step6-linkedlist/learn-LL/insert-node.js
@@ -1,3 +1,5 @@
+const prompt = require("prompt-sync")({ sigint: true });
+
 class Node {
     constructor(data1, next1) {
         this.data = data1;
@@ -5,16 +7,78 @@ class Node {
     }
 }
 
-function insertNode(head, data) {
-    let newnode = new Node(data);
-    newnode.next = head;
-    head = newnode;
+
+//insert at first -> time=O(n) and space=O(1)
+function insertNodeOnFirst(head, data) {
+    return new Node(data, head);
+}
+
+
+//insert at last -> time=O(n) and space=O(1)
+function insertAtLast(head, data) {
+    let temp = head;
+    while (temp.next) {
+        temp = temp.next;
+    }
+    temp.next = new Node(data);
     return head;
 }
 
+
+//insert at kth position -> time=O(n) and space=O(1)
+function insertAtKthPosition(head, data, k) {
+    if (head == undefined) {
+        return new Node(data);
+    }
+    if (k == 1) {
+        return new Node(data, head);
+    }
+    let temp = head;
+    let count = 0;
+    while (temp) {
+        count++;
+        if (count == k - 1) {
+            let x = new Node(data);
+            x.next = temp.next;
+            temp.next = x;
+            break;
+        }
+        temp = temp.next;
+    }
+    return head;
+}
+
+
+//insert position which match a value of LL -> time=O(n) and space=O(1)
+function insertBeforeValue(head, data, value) {
+    if (head == undefined) {
+        return new Node(data);
+    }
+    if (head.data == value) {
+        return new Node(data, head);
+    }
+    let temp = head;
+    while (temp.next) {
+        if (temp.next.data == value) {
+            let x = new Node(data);
+            x.next = temp.next;
+            temp.next = x;
+            break;
+        }
+        temp = temp.next;
+    }
+    return head;
+}
+
+
+//execute func
 let head = undefined;
-head = insertNode(head, 1);
-head = insertNode(head, 2);
-head = insertNode(head, 3);
-head = insertNode(head, 4);
-console.log("After inserting into LL, head -> ", JSON.stringify(insertNode(head, 10)));
+let length = parseInt(prompt("enter the length of which you want LL:"));
+for (let i = 0; i < length; i++) {
+    head = insertNodeOnFirst(head, parseInt(prompt("enter value:")));
+}
+console.log("LinkedList -> ", JSON.stringify(head));
+console.log("After inserting at first in LL ->", JSON.stringify(insertNodeOnFirst(head, 10)));
+console.log("After inserting at last in LL ->", JSON.stringify(insertAtLast(head, 11)));
+console.log("After inserting at kth in LL ->", JSON.stringify(insertAtKthPosition(head, 12, 4)));
+console.log("After inserting having value in LL ->", JSON.stringify(insertBeforeValue(head, 13, 5)));
