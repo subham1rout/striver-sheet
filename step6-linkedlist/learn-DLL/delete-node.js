26686924ad5698c9ec0d47358b48183e4ef5af3b
Subham Rout <subhamroutnuagaon@gmail.com>
2024-07-08 21:32:17 +0530

    add introduction to DLL

--- step6-linkedlist/learn-DLL/delete-node.js
+++ step6-linkedlist/learn-DLL/delete-node.js
@@ -27,8 +27,17 @@ function arrToDLL(arr) {
     }
     return head;
 }
+let head = arrToDLL(arr);
 
-const head = arrToDLL(arr);
+function printDLL(head){
+    let temp=head;
+    let DLL='';
+    while(temp.next){
+        DLL+=temp.data+'<->';
+        temp=temp.next;
+    }
+    console.log(DLL);
+}
 
 //delete head of DLL -> time=O(1) and space=O(1)
 function deleteHead(head) {
@@ -41,4 +50,49 @@ function deleteHead(head) {
     prev.next = undefined;
     return head;
 }
-console.log("After deleting head DLL -> ", deleteHead(head));
+head = deleteHead(head);
+console.log("After deleting head DLL -> ", printDLL(head));
+
+//delete tail of DLL -> time=O(1) and space=O(1)
+function deleteTail(head) {
+    if (head == undefined || head.next == undefined) {
+        return undefined;
+    }
+    let temp = head;
+    while (temp.next) {
+        temp = temp.next;
+    }
+    let prev = temp.back;
+    prev.next = undefined;
+    temp.back = undefined;
+    return head;
+}
+head = deleteTail(head);
+console.log("After deleting tail of DLL -> ", printDLL(head));
+
+//delete nth node of DLL -> time=O(1) and space=O(1)
+function deleteNthNode(head, n) {
+    printDLL(head)
+    if (head == undefined || head.next == undefined) {
+        return undefined;
+    }
+    let count = 0;
+    let temp = head;
+    while (temp.next) {
+        count++;
+        if (count == n) {
+            let prev = temp.back;
+            let rear = temp.next;
+            prev.next = rear;
+            rear.back = prev;
+            temp.next = undefined;
+            rear.back = undefined;
+            break;
+        }
+        temp = temp.next;
+    }
+    return head;
+}
+const n = parseInt(prompt("enter nth value="));
+head = deleteNthNode(head, n);
+console.log("After deleting nth Node of DLL -> ", printDLL(head));
--- step6-linkedlist/learn-DLL/introduction.js
+++ step6-linkedlist/learn-DLL/introduction.js
@@ -16,9 +16,19 @@ class Node {
     }
 }
 
+function printDLL(head) {
+    let temp = head;
+    let string = '';
+    while (temp) {
+        string += temp.data + '->'
+        temp = temp.next;
+    }
+    return string;
+}
+
 // Array to DLL -> time=O(n) and space=O(1)
 function arrToDLL(arr) {
-    let head = new Node(arr[0]);
+    let head = new Node(arr[0], undefined, undefined);
     let prev = head;
     for (let i = 1; i < arr.length; i++) {
         let temp = new Node(arr[i], undefined, prev);
@@ -27,4 +37,5 @@ function arrToDLL(arr) {
     }
     return head;
 }
-console.log("Array to DLL -> ", arrToDLL(arr));
+let head = arrToDLL(arr);
+console.log("Array to DLL -> ", printDLL(head));
