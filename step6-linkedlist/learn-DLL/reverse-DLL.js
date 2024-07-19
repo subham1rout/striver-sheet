cbcdc5f2ec1831fedbf6cd45bfadb16b3da799b6
Subham Rout <subhamroutnuagaon@gmail.com>
2024-07-19 22:42:44 +0530

    add brute force for reverse DLL

---
+++ step6-linkedlist/learn-DLL/reverse-DLL.js
@@ -0,0 +1,76 @@
+const prompt = require("prompt-sync")({ sigint: true });
+
+//input
+const arr = [];
+const length = parseInt(prompt("enter the length of the array:"));
+for (let i = 0; i < length; i++) {
+    arr.push(parseInt(prompt("enter number:")));
+}
+
+//Representation of DLL
+class Node {
+    constructor(data, next, back) {
+        this.data = data;
+        this.next = next;
+        this.back = back;
+    }
+}
+
+//construct DLL
+function arrToDLL(arr) {
+    let head = new Node(arr[0]);
+    let prev = head;
+    for (let i = 1; i < arr.length; i++) {
+        let temp = new Node(arr[i], undefined, prev);
+        prev.next = temp;
+        prev = temp;
+    }
+    return head;
+}
+let head = arrToDLL(arr);
+
+//print DLL
+function printDLL(head) {
+    let temp = head;
+    let DLL = '';
+    while (temp) {
+        DLL += temp.data + '<->';
+        temp = temp.next;
+    }
+    return DLL;
+}
+
+//define stack
+class Stack {
+    constructor() {
+        this.items = [];
+    }
+    push(number) {
+        this.items.push(number);
+    }
+    pop() {
+        if (this.items.length == 0) {
+            return 'no element';
+        }
+        return this.items.pop();
+    }
+}
+
+//reverse
+function reverse(head) {
+    let newstack = new Stack();
+    let temp = head;
+    while (temp) {
+        newstack.push(temp.data);
+        temp = temp.next;
+    }
+    temp = head;
+    while (temp) {
+        let val = newstack.pop();
+        temp.data = val;
+        temp = temp.next;
+    }
+    return head;
+}
+head = reverse(head);
+console.log("Reverse DLL = ", printDLL(head));
